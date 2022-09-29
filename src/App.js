c01f179a0535bda462b3f75018c07a58b19f597b
James <knotsite@gmail.com>
2022-09-29 17:08:51 +0700

    feat: add info

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>My Resume</h1>
+        <p>
+          More info please visite{" "}
+          <a href="https://github.com/napatwongchr">my github</a>
+        </p>
       </header>
     </div>
   );
